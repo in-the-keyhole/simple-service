8be6c505f0e37cf33880dc27f14b0508f6543c23
Jaime Niswonger <jniswonger@keyholesoftware.com>
2019-09-30 16:47:52 -0500

    added logging

--- src/main/java/com/keyholesoftware/devops/simpleservice/api/Api.java
+++ src/main/java/com/keyholesoftware/devops/simpleservice/api/Api.java
@@ -1,8 +1,11 @@
 package com.keyholesoftware.devops.simpleservice.api;
 
+
 import com.keyholesoftware.devops.simpleservice.client.DateServiceClient;
 import com.keyholesoftware.devops.simpleservice.model.DateTimeResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api/v1")
 public class Api {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Api.class);
+
     private DateServiceClient dateServiceClient;
 
     public Api(@Autowired DateServiceClient dateServiceClient) {
@@ -21,6 +26,7 @@ public class Api {
 
     @GetMapping(value="/hello", params= {"name"}, produces="application/json")
     public HelloResponse sayHello(@RequestParam("name") String name) {
+        LOG.debug("/api/vi/hello called with name: {}", name);
         DateTimeResponse dateTimeResponse = this.dateServiceClient.getCurrentDateTime();
         return new HelloResponse(name, dateTimeResponse.getDatetime());
     }
